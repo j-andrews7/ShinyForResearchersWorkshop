6dbde60b2768b9485d22566d34d575c84964e9da
Jared Andrews <jared.andrews07@gmail.com>
2023-05-12 13:55:38 -0500

    Fix check notes, rework first module

--- R/hist_app.R
+++ R/hist_app.R
@@ -1,8 +1,8 @@
 #' Simple Shiny app to plot a histogram
-#' 
-#' This is a simple Shiny app to plot a histogram. 
+#'
+#' This is a simple Shiny app to plot a histogram.
 #' Meant as a gentle introduction to Shiny inputs and outputs.
-#' 
+#'
 #' @param data Data frame
 #' @param x Name of column in \code{data} plot. Must be a numeric vector.
 #' @param title Title of the plot.
@@ -21,70 +21,71 @@
 #' @param cex.lab Axis label size.
 #' @param cex.axis Axis tick label size.
 #' @param cex.sub Subtitle size.
-#' 
+#'
 #' @return A Shiny app to plot a histogram.
-#' 
+#'
 #' @import shiny
 #' @importFrom graphics hist
-#' 
+#'
 #' @seealso \code{\link{hist}}
 #' @examples
 #' \dontrun{
 #' data <- data.frame(x = rnorm(100))
-#' shinyHist(data, x = "x")
+#' shiny_hist(data, x = "x")
 #' }
-#' 
+#'
 #' @author Jared Andrews
 #' @export
-shinyHist <- function(data, x, title = "Histogram", xlab = "x", ylab = "y", 
-                      main = "Histogram", col = "blue", bins = 10, xlim = range(breaks), ylim = NULL, border = "black", 
+shiny_hist <- function(data, x, title = "Histogram", xlab = "x", ylab = "y",
+                      main = "Histogram", col = "blue", bins = 10, xlim = range(bins), ylim = NULL, border = "black",
                       lwd = 1, lty = 1, cex = 1, cex.main = 1, cex.lab = 1, cex.axis = 1, cex.sub = 1) {
   # Create the UI
   ui <- fluidPage(
     # Application title
     titlePanel(title),
-    
+
     # Sidebar with a slider input for number of bins.
     sidebarLayout(
       sidebarPanel(
         sliderInput("bins",
-                    "Number of bins:",
-                    min = 1,
-                    max = 50,
-                    value = 10)
+          "Number of bins:",
+          min = 1,
+          max = 100,
+          value = bins
+        )
       ),
-      
+
       # Show a plot of the generated distribution.
       mainPanel(
         plotOutput("distPlot")
       )
     )
   )
-  
+
   # Create the server.
   server <- function(input, output) {
-    
     output$distPlot <- renderPlot({
-      x    <- data[[x]]
+      x <- data[[x]]
       hist(x,
-           col = col,
-           bins = bins,
-           xlim = xlim,
-           ylim = ylim,
-           border = border,
-           lwd = lwd,
-           lty = lty,
-           cex = cex,
-           cex.main = cex.main,
-           cex.lab = cex.lab,
-           cex.axis = cex.axis,
-           cex.sub = cex.sub,
-           xlab = xlab,
-           ylab = ylab,
-           main = main)
+        col = col,
+        bins = input$bins,
+        xlim = xlim,
+        ylim = ylim,
+        border = border,
+        lwd = lwd,
+        lty = lty,
+        cex = cex,
+        cex.main = cex.main,
+        cex.lab = cex.lab,
+        cex.axis = cex.axis,
+        cex.sub = cex.sub,
+        xlab = xlab,
+        ylab = ylab,
+        main = main
+      )
     })
   }
-  
-  # Create the application. 
+
+  # Create the application.
   shinyApp(ui = ui, server = server)
 }
